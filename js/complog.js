e9c925eb4eb74580eb2fa202bf128e5f606ce7af
Glitch (aframe) <none>
2019-11-23 20:10:49 +0000

    🍟🛳 Checkpoint ./js/complog.js:8142048/498

--- js/complog.js
+++ js/complog.js
@@ -1,16 +1,14 @@
 AFRAME.registerComponent("foo", {
   init: function() {
-    var setRot = this.el.getAttribute('rotation')+10;
-  
-       
-    this.el.setAttribute
+    
     console.log(this.el.getAttribute('rotation'));
     
   },
   update: function(){
-    el.addEventListener("xbuttondown", function() {
-      AFRAME.log("xButton Pressed", "messagez");
-    });
-    
+    var getRotation = this.el.getAttribute('rotation');
+    var setRotation = getRotation+10;
+    console.log(setRotation)
+   // el.addEventListener("abuttondown", function() {
+    //  this.el.setAttribute('rotation',{x:0 y:100 z:0});
   }
 });
