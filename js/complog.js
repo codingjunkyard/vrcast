a5238667607b04665c355e8653bb276aef8a659d
Glitch (aframe) <none>
2019-11-23 22:18:49 +0000

    👙🐉 Checkpoint ./js/rotate-player.js:8142048/1489 ./index.html:8142048/188 ./js/complog.js:8142048/458

--- js/complog.js
+++ js/complog.js
@@ -1,22 +1,10 @@
-AFRAME.registerComponent("foo", {
-  update: function() {
+AFRAME.registerComponent("vrlog", {
+  init: function() {
     var camRig = this.el.querySelector("#cameraRig");
     var camRigRot = this.el
       .querySelector("#cameraRig")
       .getAttribute("rotation");
-    var y = camRigRot.y;
-
-    this.el.addEventListener("abuttondown", function(evt1) {
-      AFRAME.log("a Button Pressed");
-      camRig.setAttribute("rotation", { x: 0, y: y + 10, z: 0 }, true);
-      AFRAME.log(camRigRot);
-    });
+        
     AFRAME.log(camRigRot);
-
-    this.el.addEventListener("xbuttondown", function(evt1) {
-      AFRAME.log("x Button Pressed");
-      camRig.setAttribute("rotation", { x: 0, y: y - 10, z: 0 }, true);
-      AFRAME.log(camRigRot);
-    });
   }
 });
--- js/rotate-player.js
+++ js/rotate-player.js
@@ -1,25 +1,18 @@
 AFRAME.registerComponent("rotate-player", {
   
-  
-  
   update: function() {
-    var camRig = this.el.querySelector("#cameraRig");
-    var camRigRot = this.el
-      .querySelector("#cameraRig")
-      .getAttribute("rotation");
-    var y = camRigRot.y;
+    
+  var y=this.el.getAttribute('rotation').y;
 
     this.el.addEventListener("abuttondown", function(evt1) {
-      AFRAME.log("a Button Pressed");
-      camRig.setAttribute("rotation", { x: 0, y: y + 10, z: 0 }, true);
-      AFRAME.log(camRigRot);
+      this.setAttribute("rotation", { x: 0, y: y+=10, z: 0 });
+      
     });
-    AFRAME.log(camRigRot);
-
+    
     this.el.addEventListener("xbuttondown", function(evt1) {
-      AFRAME.log("x Button Pressed");
-      camRig.setAttribute("rotation", { x: 0, y: y - 10, z: 0 }, true);
-      AFRAME.log(camRigRot);
+           this.setAttribute("rotation", { x: 0, y: y-=10, z: 0 });
+      
+
     });
   }
 });
