c55c2a59db5a62b7ff5cd9d99c5d0a0d2d6c729b
Glitch (aframe) <none>
2019-11-23 15:18:14 +0000

    🚪🎯 Checkpoint ./js/aLog.js:8142048/2137

--- js/aLog.js
+++ js/aLog.js
@@ -1,31 +1,49 @@
+AFRAME.registerComponent("hudlog", {
+  schema: {
+    message: { type: "string", default: "Hello, World!" }
+  },
+  
+  init: function() {
+          
+    var self = this;
 
-      AFRAME.registerComponent('hudlog', {
-        init: function () {
-          var self = this;
+    // Using setInterval for demo convenience, but in practice, should use tick.
+    setInterval(function() {
+      self.logRandomColor();
+    }, 500);
 
-          // Using setInterval for demo convenience, but in practice, should use tick.
-          setInterval(function () {
-            self.logRandomColor();
-          }, 500);
+    setInterval(function() {
+      self.logRandomNumber();
+    }, 250);
 
-          setInterval(function () {
-            self.logRandomNumber();
-          }, 250);
+    this.el.emit("log", { message: "Logging started!" });
+  },
 
-          this.el.emit('log', {message: 'Logging started!'});
-        },
+  logRandomColor: function() {
+    var colors = [
+      "red",
+      "orange",
+      "yellow",
+      "green",
+      "blue",
+      "indigo",
+      "violet"
+    ];
+    AFRAME.log(colors[Math.floor(Math.random() * 7)], "colorz");
+  },
 
-        logRandomColor: function () {
-          var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-          AFRAME.log(colors[Math.floor(Math.random() * 7)], 'colorz');
-        },
+  logRandomNumber: function() {
+    AFRAME.log(Math.random() * 10000, "numberz");
+  },
 
-        logRandomNumber: function () {
-          AFRAME.log(Math.random() * 10000, 'numberz');
-        },
-
-        tick: function (t) {
-          AFRAME.log('Time: ' + t, 'time');
-        }
-      });
-    
+  tick: function(t) {
+    AFRAME.log("Time: " + t, "time");
+  },
+  
+  logMessage: function(){
+    AFRAME.log(this.data.message, "messagez");
+   }
+  
+  
+  
+});
