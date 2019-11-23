20781b7c0562f1bd5a0bfad003fdc1f6339f84f1
Glitch (aframe) <none>
2019-11-23 21:22:49 +0000

    🎨💠 Checkpoint ./js/complog.js:8142048/512

--- js/complog.js
+++ js/complog.js
@@ -1,24 +1,16 @@
 AFRAME.registerComponent("foo", {
-    
-  init: function() {
-     var sceneEl = document.querySelector('a-scene');
-     var camRigRot = sceneEl.querySelector('#cameraRig').getAttribute('rotation');
-     console.log(camRigRot);
-     AFRAME.log(camRigRot);
-  },
-  
+   
   update: function() {
-    var sceneEl = document.querySelector('a-scene');
-    var camRig
-    var camRigRot = sceneEl.querySelector('#cameraRig').getAttribute('rotation');
+    var camRig = this.el.querySelector('#cameraRig');
+    var camRigRot = this.el.querySelector('#cameraRig').getAttribute('rotation');
     var y = (camRigRot.y)+10;
     
    this.el.addEventListener("abuttondown", function(evt1) {
-         sceneEl.('#cameraRig').setAttribute('rotation', {x: 0, y: y, z: 0});
+         camRig.setAttribute('rotation', {x: 0, y: y, z: 0});
    });
     
-         console.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
-    AFRAME.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
+    console.log(camRigRot);
+    AFRAME.log(camRigRot);
    }
                                                   
 });
