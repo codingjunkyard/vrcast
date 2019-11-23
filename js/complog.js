e4aa132225c06cc03b46194df461fba395b26ac4
Glitch (aframe) <none>
2019-11-23 21:10:49 +0000

    📷🚲 Checkpoint ./js/complog.js:8142048/985

--- js/complog.js
+++ js/complog.js
@@ -1,22 +1,20 @@
 AFRAME.registerComponent("foo", {
-  
-  var rigEl = document.querySelector('a-scene');
-console.log(sceneEl.querySelector('#redBox'));
-  
-  
-  init: function() {
-        console.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
-        AFRAME.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
     
+  init: function() {
+     var sceneEl = document.querySelector('a-scene');
+     var camRigRot = sceneEl.querySelector('#cameraRig').getAttribute('rotation');
+     console.log(camRigRot);
+     AFRAME.log(camRigRot);
   },
   
   update: function() {
-    var x = 0, 
-        y = (this.el.querySelector('#cameraRig').getAttribute('rotation').y)+10, 
-        z = 0;
+    var sceneEl = document.querySelector('a-scene');
+    var camRig
+    var camRigRot = sceneEl.querySelector('#cameraRig').getAttribute('rotation');
+    var y = (camRigRot.y)+10;
     
    this.el.addEventListener("abuttondown", function(evt1) {
-         this.el.querySelector('#cameraRig').setAttribute('rotation', {x: x, y: y, z: z});
+         sceneEl.('#cameraRig').setAttribute('rotation', {x: 0, y: y, z: 0});
    });
     
          console.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
