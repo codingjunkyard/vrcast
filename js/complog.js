da879ca028a2d0fad7d56f3a5e8a6f1b8caa69c9
Glitch (aframe) <none>
2019-11-23 20:58:50 +0000

    🏡👸 Checkpoint ./js/complog.js:8142048/516

--- js/complog.js
+++ js/complog.js
@@ -1,21 +1,26 @@
 AFRAME.registerComponent("foo", {
+  
+  var rigEl = document.querySelector('a-scene');
+console.log(sceneEl.querySelector('#redBox'));
+  
+  
   init: function() {
-        console.log("Old Rotation"+this.el.querySelector('#cameraRig').getAttribute('rotation'));
+        console.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
         AFRAME.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
     
   },
   
   update: function() {
     var x = 0, 
-        y = (this.el.getAttribute('rotation').y)+10, 
+        y = (this.el.querySelector('#cameraRig').getAttribute('rotation').y)+10, 
         z = 0;
     
    this.el.addEventListener("abuttondown", function(evt1) {
-         this.el.setAttribute('rotation', {x: x, y: y, z: z});
+         this.el.querySelector('#cameraRig').setAttribute('rotation', {x: x, y: y, z: z});
    });
     
-         console.log(this.el.getAttribute('rotation'));
-    AFRAME.log(this.el.getAttribute('rotation'));
+         console.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
+    AFRAME.log(this.el.querySelector('#cameraRig').getAttribute('rotation'));
    }
                                                   
 });
