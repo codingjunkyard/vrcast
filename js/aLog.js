7bedf804c5cc7f30a7566d974f2d89326e33db12
Glitch (aframe) <none>
2019-11-23 14:42:06 +0000

    👵🐥 Checkpoint ./index.html:8142048/18700 ./js/aLog.js:8142048/890

---
+++ js/aLog.js
@@ -0,0 +1,31 @@
+
+      AFRAME.registerComponent('main', {
+        init: function () {
+          var self = this;
+
+          // Using setInterval for demo convenience, but in practice, should use tick.
+          setInterval(function () {
+            self.logRandomColor();
+          }, 500);
+
+          setInterval(function () {
+            self.logRandomNumber();
+          }, 250);
+
+          this.el.emit('log', {message: 'Logging started!'});
+        },
+
+        logRandomColor: function () {
+          var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+          AFRAME.log(colors[Math.floor(Math.random() * 7)], 'colorz');
+        },
+
+        logRandomNumber: function () {
+          AFRAME.log(Math.random() * 10000, 'numberz');
+        },
+
+        tick: function (t) {
+          AFRAME.log('Time: ' + t, 'time');
+        }
+      });
+    
