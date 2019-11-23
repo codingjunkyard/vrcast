e327fbbb3685960555eaa43e656f6af57b918d3f
Glitch (aframe) <none>
2019-11-23 22:30:49 +0000

    🐁🎤 Checkpoint ./js/complog.js:8142048/1567

--- js/complog.js
+++ js/complog.js
@@ -1,10 +1,14 @@
 AFRAME.registerComponent("vrlog", {
-  init: function() {
-    var camRig = this.el.querySelector("#cameraRig");
-    var camRigRot = this.el
-      .querySelector("#cameraRig")
-      .getAttribute("rotation");
-        
-    AFRAME.log(camRigRot);
-  }
-});
+  update: function() {
+     var el = this.el; // Reference to the component's entity.
+
+    el.addEventListener("xbuttondown", function(evt1) {
+      AFRAME.log("xButton Pressed");
+      AFRAME.log(el.querySelector);
+    });
+    
+    el.addEventListener("abuttondown", function(evt1) {
+      AFRAME.log("aButton Pressed");
+    });
+  },
+})
