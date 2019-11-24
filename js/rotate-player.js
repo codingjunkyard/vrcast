b62a8c0f4438c4123246099497cb436e15996685
Glitch (aframe) <none>
2019-11-24 05:58:14 +0000

    🚶👙 Checkpoint ./js/rotate-player.js:8142048/646

--- js/rotate-player.js
+++ js/rotate-player.js
@@ -2,13 +2,17 @@ AFRAME.registerComponent("rotate-player", {
   
   update: function() {
   var y=this.el.getAttribute('rotation').y;
-  <!--TODO: var playerAffector=querySelector('a-scene').querySelector(#cameraRig)-->
+  //TODO: var playerAffector=querySelector('a-scene').querySelector(#cameraRig)
 
     this.el.addEventListener("abuttondown", function(evt1) {
+      //this.el.addEventListener("axismove", function(evt1) {
+      //if (evt1.detail.axis[0]>0.7){
+      //playerAffector.setAttribute("rotation", { x: 0, y: y-=10, z: 0 });
+      //}
       this.setAttribute("rotation", { x: 0, y: y-=10, z: 0 });
       if(y==360){
         y=0;
-      }
+}
     });
     
     this.el.addEventListener("bbuttondown", function(evt1) {
