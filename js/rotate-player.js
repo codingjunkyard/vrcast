806b6c3132a37f38c22df228dd02180cf130499f
Glitch (rotateplayer) <none>
2019-11-28 17:54:15 +0000

    💵🐉 Checkpoint ./js/rotate-player.js:8142048/1986 ./js/complog.js:8142048/14

--- js/rotate-player.js
+++ js/rotate-player.js
@@ -1,29 +1,29 @@
 AFRAME.registerComponent("rotate-player", {
-  
   update: function() {
-   console.log(this);
-  var y=this.el.getAttribute('rotation').y;
-  //var playerAffector=this.el
+    console.log(this.el);
+    var y = this.el.querySelector("#cameraRig").getAttribute("rotation").y;
+    //console.log(this.el.querySelector('#right-hand'));
+    var playerAffector = this.el.querySelector("#right-hand");
 
-    //this.el.addEventListener("abuttondown", function(evt1) { 
-      //AFRAME.log(this.el);
-      this.el.addEventListener("axismove", function(evt1) {
-        AFRAME.log(evt1.detail.axis);
-      if (evt1.detail.axis[0]>0.7){
-      //playerAffector.setAttribute("rotation", { x: 0, y: y-=10, z: 0 });
-      //}
-      this.setAttribute("rotation", { x: 0, y: y-=10, z: 0 });
-      if(y==360){
-        y=0;}
+    //this.el.addEventListener("abuttondown", function(evt1) {
+    //AFRAME.log(this.el);
+    playerAffector.addEventListener("axismove", function(evt1) {
+      AFRAME.log(evt1.detail.axis);
+      if (evt1.detail.axis[0] > 0.7) {
+        //playerAffector.setAttribute("rotation", { x: 0, y: y-=10, z: 0 });
+        //}
+        this.setAttribute("rotation", { x: 0, y: (y -= 10), z: 0 });
+        if (y == 360) {
+          y = 0;
+        }
       }
     });
-    
-    this.el.addEventListener("bbuttondown", function(evt2) {
-           this.setAttribute("rotation", { x: 0, y: y+=10, z: 0 });
-      if(y==-360){
-        y=0;
-      }
 
+    this.el.addEventListener("bbuttondown", function(evt2) {
+      this.setAttribute("rotation", { x: 0, y: (y += 10), z: 0 });
+      if (y == -360) {
+        y = 0;
+      }
     });
   }
 });
