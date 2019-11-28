da92e2ab81c959e82554fe30e72ea4ef4d415fe3
Glitch (rotateplayer) <none>
2019-11-28 20:35:12 +0000

    🐆🎖 Checkpoint ./index.html:8142048/165 ./js/rotate-player.js:8142048/2379

--- js/rotate-player.js
+++ js/rotate-player.js
@@ -2,14 +2,6 @@ AFRAME.registerComponent("rotate-player", {
   schema: {
     hand: { default: "#right-hand", oneOf: ["#right-hand", "#left-hand"] }
   },
-  //gamepad-controls="controller: 1"
-  init: function() {
-    var gp = navigator.getGamepads();
-    console.log(gp);
-    AFRAME.log(gp);
-    //AFRAME.log(navigator.getGamepads().id);
-  },
-
   update: function() {
     var data = this.data;
     var playerGetter = document.querySelector("#cameraRig");
@@ -25,7 +17,6 @@ AFRAME.registerComponent("rotate-player", {
 
     playerAffector.addEventListener("axismove", function(evt1) {
       AFRAME.log(evt1.detail.axis);
-      AFRAME.log(navigator.getGamepads().id);
       if (evt1.detail.axis[0] > 0.7) {
         playerGetter.setAttribute("rotation", { x: 0, y: (y -= 10), z: 0 });
         if (y == 360) {
