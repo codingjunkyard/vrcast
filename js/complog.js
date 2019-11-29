23c1fdddbb71f995c8c93eaab5781d4ddfff0835
Glitch (rotateplayer) <none>
2019-11-29 14:16:32 +0000

    🌧📅 Checkpoint ./index.html:8142048/22 ./js/complog.js:8142048/1109

--- js/complog.js
+++ js/complog.js
@@ -2,37 +2,17 @@ const COMPONENT_SUFFIX = '-controls';
 
 AFRAME.registerComponent("vrlog", {
   init: function(){
-    //var contollerArray = navigator.getGamepads()[0].id;
-    var fooComponent = this.el.querySelector('[gamepad-controls]');
-console.log(fooComponent);
-//fooComponent.qux();
-    
+        
     console.log(this.el.querySelector('#cameraRig'));
-    var el = this.el; // Reference to the component's entity.
-    
-    AFRAME.log("Look at your controllers");
-    AFRAME.log(el.querySelector('#cameraRig').getAttribute('rotation'));
-    //AFRAME.log(controllerArray);
-    
+        
+    AFRAME.log("Hello From vrlog");
+   // AFRAME.log(el.querySelector('#cameraRig').getAttribute('rotation'));
+       
         
   },
   update: function() {
-     var el = this.el; // Reference to the component's entity.
-
-    el.addEventListener("xbuttondown", function(evt1) {
-      AFRAME.log("xButton Pressed");
-      AFRAME.log(el.querySelector('#cameraRig').getAttribute('rotation'));
-    });
+     
     
-    el.addEventListener("abuttondown", function(evt1) {
-      AFRAME.log("aButton Pressed");
-      AFRAME.log(el.querySelector('#cameraRig').getAttribute('rotation'));
-    });
-    
-    el.addEventListener("axismove", function(evt2) {
-      AFRAME.log('axismove',evt2.detail.axis);
-      AFRAME.log(el.querySelector('#cameraRig').getAttribute('rotation'));
-    });
     
   },
 });
