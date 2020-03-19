3f5094fed6ad57cb9497c4c7c25e42fedbb4080e
Glitch (rotateplayer) <none>
2020-03-19 21:55:50 +0000

    🌸🏗 Checkpoint ./public/js/spawnAudience.js:8142048/755 ./.glitch-assets:8142048/371 ./public/index.html:8142048/260

--- public/js/spawnAudience.js
+++ public/js/spawnAudience.js
@@ -1,4 +1,4 @@
-count = count + 1;
+
 AFRAME.registerComponent('spawnaudience', {
   schema: {
     radius: {type: 'number', default: 1}
@@ -7,31 +7,16 @@ AFRAME.registerComponent('spawnaudience', {
   init: function() {
     var el = this.el;
     var center = el.getAttribute('position');
-    var nextSeat = count; 
-    var angleRad = this.getRandomAngleInRadians();
-    var circlePoint = this.randomPointOnCircle(this.data.radius, angleRad);
-    var players = document.querySelectorAll(".ball");
-    var worldPoint = {x: circlePoint.x + center.x, y: center.y+1.2, z: circlePoint.y + center.z};
+    var players = document.querySelectorAll(".head");
+    var worldPoint = {x: center.x + players.length + 1, y: 0, z: 0};
     
     el.setAttribute('position', worldPoint);
     console.log('world point', worldPoint);
 
-    var angleDeg = angleRad * 180 / Math.PI;
-    var angleToCenter = -1 * angleDeg + 90;
-    var angleRad = THREE.Math.degToRad(angleToCenter);
-    el.object3D.rotation.set(0, angleRad, 0);
-    console.log('angle deg', angleDeg);
-    console.log('count', count);
-  },
-
-  getRandomAngleInRadians: function() {
-    return Math.random()*Math.PI*2;
-  },
-
-  randomPointOnCircle: function (radius, angleRad) {
-    var x = Math.cos(angleRad)*radius;
-    var y = Math.sin(angleRad)*radius;
-    return {x: x, y: y};
+    
+    el.object3D.rotation.set(0, 0, 0);
+    
+    console.log('count', players.length);
   }
   
 });
