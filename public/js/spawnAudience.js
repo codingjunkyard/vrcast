f423883cfea4128b2773e2b232b0f8c59f72d738
Glitch (rotateplayer) <none>
2020-03-21 02:52:39 +0000

    👙🍗 Checkpoint ./public/js/spawnAudience.js:8142048/1807 ./public/index.html:8142048/117

--- public/js/spawnAudience.js
+++ public/js/spawnAudience.js
@@ -1,22 +1,41 @@
 
-AFRAME.registerComponent('spawnaudience', {
+/**
+ * Creates a grid of entities based on the given template
+ * Place at the center of the grid you wish to create
+ * Requires aframe-template-component
+ */
+AFRAME.registerComponent('spawn-grid', {
   schema: {
-    radius: {type: 'number', default: 1}
+    clone: {type: 'string'},
+    rows: {default: 2},
+    rowSeparation: {default: 2},
+    columns: {default: 2},
+    columnSeparation: {default: 2}
   },
 
   init: function() {
-    var el = this.el;
-    var center = el.getAttribute('position');
-    var players = document.querySelectorAll(".head");
-    var worldPoint = {x: center.x + players.length + 1, y: 0, z: 0};
-    
-    el.setAttribute('position', worldPoint);
-    console.log('world point', worldPoint);
+    this.clone = document.querySelector(this.data.clone);
+    var y = this.el.getAttribute('position').y;
+    for (var i = 0; i < this.data.rows; i++) {
+      var z = i * this.data.rowSeparation - (this.data.rowSeparation * this.data.rows / 2) + this.data.rowSeparation / 2;
+      for (var j = 0; j < this.data.columns; j++) {
+        var x = j * this.data.columnSeparation - (this.data.columnSeparation * this.data.columns / 2) + this.data.columnSeparation / 2;
+        this.spawnEntity(i, j, x, y, z);
+      }
+    }
+  },
 
-    
-    el.object3D.rotation.set(0, 0, 0);
-    
-    console.log('count', players.length);
+  spawnEntity: function(row, col, x, y, z) {
+    var cloned = this.clone.cloneNode(false);
+    this.el.appendChild(cloned);
+
+    var id = cloned.getAttribute('id');
+    var newId = id + '-' + row + '-'+ col;
+    cloned.setAttribute('id', newId);
+
+    var position = x + ' ' + y + ' ' + z;
+    cloned.setAttribute('position', position);
+
+    cloned.setAttribute('visible', true);
   }
-  
 });
